a218844bcebe1fade18e45c679774fda6eb59bb7
naiden nikolow <naiden452@gmail.com>
2021-03-28 22:51:34 +0300

    Add project files.

---
+++ SDL/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "SDL.h"
+
+int main(int argc, char* argv[]) 
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* initwindow = SDL_CreateWindow("Everything", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(initwindow, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer); 
+	SDL_Delay(3000);
+
+	return 0;
+}
